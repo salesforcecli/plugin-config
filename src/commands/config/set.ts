411d683f2fe5c7b5d40ceb8a56ac45b0854dc88e
colby-matthiasson <colby.matthiasson@salesforce.com>
2020-07-09 08:12:19 -0700

    redesigned config:set

--- src/commands/config/set.ts
+++ src/commands/config/set.ts
@@ -9,31 +9,20 @@
 import * as _ from 'lodash';
 
 import { flags, FlagsConfig, SfdxCommand } from '@salesforce/command';
-import {
-  Messages,
-  SfdxError,
-  Config,
-  Org,
-  ConfigContents
-} from '@salesforce/core';
-import { Dictionary } from '@salesforce/ts-types';
+import { Config, Messages, Org } from '@salesforce/core';
 
 Messages.importMessagesDirectory(__dirname);
 const messages = Messages.loadMessages('@salesforce/plugin-config', 'set');
 
-const VALID_CONFIG: Dictionary<
-  (key: string, value: string, isGlobal: boolean) => Promise<ConfigContents>
-> = {};
+// interface SuccessMsg {
+//   name: string;
+//   value: string;
+// }
 
-interface SuccessMsg {
-  name: string;
-  value: string;
-}
-
-interface FailureMsg {
-  name: string;
-  message: string;
-}
+// interface FailureMsg {
+//   name: string;
+//   message: string;
+// }
 
 export class Set extends SfdxCommand {
   public static readonly theDescription = messages.getMessage(
@@ -54,49 +43,26 @@ export class Set extends SfdxCommand {
     })
   };
 
-  private successes: SuccessMsg[] = [];
-  private failures: FailureMsg[] = [];
+  // private successes: SuccessMsg[] = [];
+  // private failures: FailureMsg[] = [];
 
-  public async run(): Promise<unknown> {
-    await this.validate();
+  public async run(): Promise<void> {
     for (const name of Object.keys(this.varargs!)) {
       const value: string = (this.varargs![name] as unknown) as string;
-      try {
-        await VALID_CONFIG[name]!(name, value, this.flags.global);
-        this.successes.push({ name, value });
-      } catch (err) {
-        this.failures.push({ name, message: err.message });
+      if (
+        name === Config.DEFAULT_DEV_HUB_USERNAME ||
+        name === Config.DEFAULT_USERNAME
+      ) {
+        await Org.create({ aliasOrUsername: value });
       }
+      await Set.setConfig(name, value, this.flags.global);
     }
-    return {};
   }
-
-  async validate() {
-    [Config.DEFAULT_DEV_HUB_USERNAME, Config.DEFAULT_USERNAME].forEach(type => {
-      VALID_CONFIG[type] = this.setOrgDefault;
-    });
-
-    Config.getAllowedProperties().forEach(prop => {
-      if (_.isNil(VALID_CONFIG[prop.key])) {
-        VALID_CONFIG[prop.key] = this.setConfig;
-      }
-    });
-
-    Object.keys(this.varargs!).forEach(key => {
-      if (!_.isFunction(VALID_CONFIG[key])) {
-        throw SfdxError.create(
-          '@salesforce/core',
-          'config',
-          'UnknownConfigKey',
-          [key]
-        );
-      }
-    });
-
-    return;
-  }
-
-  async setConfig(key: string, value: string, isGlobal: boolean = false) {
+  static async setConfig(
+    key: string,
+    value: string,
+    isGlobal: boolean = false
+  ) {
     let config: Config;
     try {
       config = await Config.create(Config.getDefaultOptions(isGlobal));
@@ -114,16 +80,5 @@ export class Set extends SfdxCommand {
         config.set(key, value);
       })
       .then(() => config.write());
-  }
-
-  setOrgDefault(type: string, username: string, isGlobal: boolean) {
-    if (_.isNil(username)) {
-      return this.setConfig(type, username, isGlobal);
-    } else {
-      // Ensure the username exits by getting the config.
-      return Org.create({ aliasOrUsername: username }).then(() =>
-        this.setConfig(type, username, isGlobal)
-      );
-    }
   }
 }
