0d539349dc8c9c0a66fb9e95956dc0e2458eb0a6
Chad Carbert <chadcarbert@me.com>
2020-10-28 12:36:58 -0400

    Works when ran directly: npx mocha -r ts-node/register test/commands/config/set.test.ts
    But fails with:
    yarn test

--- test/commands/config/set.test.ts
+++ test/commands/config/set.test.ts
@@ -5,63 +5,11 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { $$, expect, test } from '@salesforce/command/lib/test';
-import { Config, Org } from '@salesforce/core';
-import { StubbedType, stubInterface, stubMethod } from '@salesforce/ts-sinon';
-import { SinonStub } from 'sinon';
+import { expect, test } from '@oclif/test';
+import { Config } from '@salesforce/core';
 
 describe('config:set', () => {
-  let configStub: StubbedType<Config>;
-  let orgStub: StubbedType<Org>;
-  let orgCreateSpy: SinonStub;
-
-  async function prepareStubs() {
-    configStub = stubInterface<Config>($$.SANDBOX, {});
-    stubMethod($$.SANDBOX, Config, 'create').callsFake(async () => configStub);
-  }
-
   test
-    .do(async () => await prepareStubs())
-    .stdout()
-    .command(['config:set', `${Config.API_VERSION}=49.0`, '--global', '--json'])
-    .it('should return values for all configured properties', (ctx) => {
-      const result = JSON.parse(ctx.stdout).result;
-      expect(result.successes).to.deep.equal([{ name: Config.API_VERSION, value: '49.0' }]);
-      expect(configStub.set.callCount).to.equal(1);
-    });
-
-  test
-    .do(async () => {
-      await prepareStubs();
-      orgStub = stubInterface<Org>($$.SANDBOX, {});
-      orgCreateSpy = stubMethod($$.SANDBOX, Org, 'create').callsFake(async () => orgStub);
-    })
-    .stdout()
-    .command(['config:set', `${Config.DEFAULT_USERNAME}=MyUser`, '--global', '--json'])
-    .it('should instantiate an Org when defaultusername is set', (ctx) => {
-      const result = JSON.parse(ctx.stdout).result;
-      expect(result.successes).to.deep.equal([{ name: Config.DEFAULT_USERNAME, value: 'MyUser' }]);
-      expect(configStub.set.callCount).to.equal(1);
-      expect(orgCreateSpy.callCount).to.equal(1);
-    });
-
-  test
-    .do(async () => {
-      await prepareStubs();
-      orgStub = stubInterface<Org>($$.SANDBOX, {});
-      orgCreateSpy = stubMethod($$.SANDBOX, Org, 'create').callsFake(async () => orgStub);
-    })
-    .stdout()
-    .command(['config:set', `${Config.DEFAULT_DEV_HUB_USERNAME}=MyDevhub`, '--global', '--json'])
-    .it('should instantiate an Org when defaultusername is set', (ctx) => {
-      const result = JSON.parse(ctx.stdout).result;
-      expect(result.successes).to.deep.equal([{ name: Config.DEFAULT_DEV_HUB_USERNAME, value: 'MyDevhub' }]);
-      expect(configStub.set.callCount).to.equal(1);
-      expect(orgCreateSpy.callCount).to.equal(1);
-    });
-
-  test
-    .do(async () => await prepareStubs())
     .stdout()
     .command(['config:set', `${Config.DEFAULT_USERNAME}=NonExistentOrg`, '--global', '--json'])
     .it('should handle failed org create with --json flag', (ctx) => {
@@ -73,7 +21,6 @@ describe('config:set', () => {
     });
 
   test
-    .do(async () => await prepareStubs())
     .stdout()
     .command(['config:set', `${Config.DEFAULT_USERNAME}=NonExistentOrg`, '--global'])
     .it('should handle failed org create with no --json flag', (ctx) => {
